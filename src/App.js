16b3e2e525a001d212b8c77bb54d8100895ec057
ChinniSaiteja <chinnisaiteja8688@gmail.com>
2024-05-01 17:45:04 +0530

    j

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import Header from './components/Header'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Coins from "./components/Coins";
+import Exchanges from "./components/Exchanges";
+import CoinDetails from "./components/CoinDetails";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <>
-      <Router>
-        <Header/>
-        <Routes>
-          <Route/>
-        </Routes>
-      </Router>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {ChakraProvider } from '@chakra-ui/react';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <ChakraProvider>
-            <App/>
-        </ChakraProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
+
+export const server = `https://api.coingecko.com/api/v3`;
